6f2c3da0b7dfeaceed07b79469ef1e085d4ad854
tiberedbzt <tibere.dbzt@gmail.com>
2025-12-05 11:23:23 +0100

    :label: Create ChatApi interface

---
+++ client/src/features/chat/api/chatApi.ts
@@ -0,0 +1,16 @@
+import type { Message, JoinRoomResponse } from '@chat/types';
+
+export interface ChatApi {
+  connect(): void;
+  disconnect(): void;
+  isConnected(): boolean;
+  
+  joinRoom(username: string, roomName: string): Promise<JoinRoomResponse>;
+  sendMessage(content: string, username: string, userId: string, roomName: string): void;
+  
+  onMessage(callback: (message: Message) => void): void;
+  onUserJoined(callback: (data: { users: string[] }) => void): void;
+  
+  offMessage(callback: (message: Message) => void): void;
+  offUserJoined(callback: (data: { users: string[] }) => void): void;
+}
