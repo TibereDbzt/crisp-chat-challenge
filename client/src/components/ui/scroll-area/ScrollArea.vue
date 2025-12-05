f4eb422c3514a77750c69c98a29e7a761a320532
tiberedbzt <tibere.dbzt@gmail.com>
2025-12-05 11:22:07 +0100

    :lipstick: Add shadcn components

---
+++ client/src/components/ui/scroll-area/ScrollArea.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { ScrollAreaCorner, ScrollAreaRoot, ScrollAreaViewport } from 'radix-vue'
+import type { HTMLAttributes } from 'vue'
+import { cn } from '@/lib/utils'
+import ScrollBar from './ScrollBar.vue'
+
+const props = withDefaults(
+  defineProps<{
+    class?: HTMLAttributes['class']
+    orientation?: 'vertical' | 'horizontal'
+  }>(),
+  {
+    orientation: 'vertical',
+  },
+)
+</script>
+
+<template>
+  <ScrollAreaRoot :class="cn('relative overflow-hidden', props.class)">
+    <ScrollAreaViewport class="h-full w-full rounded-[inherit]">
+      <slot />
+    </ScrollAreaViewport>
+    <ScrollBar :orientation="orientation" />
+    <ScrollAreaCorner />
+  </ScrollAreaRoot>
+</template>
